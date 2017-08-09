c0219003c5fbfec3aeac3682d4ab764d2013edd4
Cabeus <cabeus@qq.com>
2017-08-09 15:05:15 +0800

    1

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ require("../css/mian.css")
 
 
 document.write("<div>你可是出来了</div>");
-document.write("<div>你可是出来了</div>");
 document.write("<h1>你可是出来了</h1>");
+document.write("<h2>你可是出来了</h2>");
 
 const aaa = require('./student.json');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-	entry: __dirname + '/src/js/index.js',//链接js文件
+	entry: __dirname + '/src/js/index.js', //链接js文件
 	output: {
-		path: __dirname + '/assets/js/',//打包js文件地址
-		filename: "index123.js",  //打包的js文件名字
+		path: __dirname + '/assets/js/', //打包js文件地址
+		filename: "index123.js", //打包的js文件名字
 		publicPath: '/assets/js/'
 	},
-	devServer: {}, 
-	module: {  //模块
-			rules: [{
-				test: /\.(css|less)$/,
-				use: ['style-loader', 'css-loader','less-loader']
-			}],
-			 loaders: [{
-			  	test: /\.json$/,
-				loader: 'json-loader'
+	devServer: {},
+	module: { //模块
+		rules: [{
+			test: /\.(css|less)$/,
+			use: ['style-loader', 'css-loader', 'less-loader']
+		}, {
+			test: /\.(png|jpg|gif)$/,
+			use: [{
+				loader: 'url-loader',
+				options: {
+					limit: 8192
+				}
 			}]
-		}
+		}],
+		loaders: [{
+			test: /\.json$/,
+			loader: 'json-loader'
+		}]
+	}
 }
