91bd89a40d43015ba5e1ccffa173e81e629b23a9
Cabeus <cabeus@qq.com>
2017-08-09 11:53:30 +0800

    更新

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
+require("../css/mian.css")
+
+
+document.write("<div>你特码的可是出来了</div>");
 document.write("<div>你特码的可是出来了</div>")
+document.write("<h1>你特码的可是出来了</h1>")
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-	entry: __dirname + '/src/js/index.js',
-	output:{
-		path: __dirname + '/assets/js/',
-		filename:"index123.js"
-	}
+	entry: __dirname + '/src/js/index.js',//链接js文件
+	output: {
+		path: __dirname + '/assets/js/',//打包js文件地址
+		filename: "index123.js",  //打包的js文件名字
+		publicPath: '/assets/js/'
+	},
+	devServer: {}, //开发中 Server，需要安装 webpack-dev-server
+	module: {  //模块
+			rules: [{
+				test: /\.(css|less)$/,
+				use: ['style-loader', 'css-loader','less-loader']
+			}]
+		}
 }
